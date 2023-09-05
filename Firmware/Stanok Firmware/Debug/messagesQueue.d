a05ef98aa659f9afa2c81f70de52c4a7182d3ad0
IgorPetrov0 <mizantrop_0@mail.ru>
2023-09-05 05:32:32 +0300

    z
    z

--- Firmware/Stanok Firmware/Debug/messagesQueue.d
+++
@@ -1,7 +0,0 @@
-messagesQueue.d messagesQueue.o: .././messagesQueue.c \
- .././messagesQueue.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
-
-.././messagesQueue.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
